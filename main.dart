f909aea708090cea5f693e9e5c7615ed4d1320a4
JohnnyAppAkademie <jonathan.gur@protonmail.com>
2025-07-01 13:18:57 +0200

    Add abstract class "DatabaseRepo"

--- main.dart
+++ main.dart
@@ -1,3 +1,10 @@
+abstract class DatabaseRepository {
+  String readData();
+  void writeData(String data);
+  void changeData(String data);
+  void deleteData(String data);
+}
+
 int main() {
   return 0;
 }
