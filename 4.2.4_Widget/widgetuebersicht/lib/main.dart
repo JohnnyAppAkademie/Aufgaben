41cb997aa13be788845654c3cb612ede4819dfb0
JohnnyAppAkademie <jonathan.gur@protonmail.com>
2025-07-09 14:30:34 +0200

    Commit - 4.2.4 Bonus Aufgabe

--- 4.2.4_Widget/widgetuebersicht/lib/main.dart
+++ 4.2.4_Widget/widgetuebersicht/lib/main.dart
@@ -4,6 +4,66 @@ void main() {
   runApp(const MainApp());
 }
 
+Widget welcomeTextBar() {
+  return Text(
+    "Hello App Akademie!",
+    style: TextStyle(
+      color: Colors.blue,
+      fontSize: 18,
+      fontWeight: FontWeight.w900,
+      fontStyle: FontStyle.normal,
+    ),
+    textAlign: TextAlign.start,
+    textDirection: TextDirection.ltr,
+  );
+}
+
+Widget trippleContainerRow() {
+  return Row(
+    spacing: 15,
+    children: [
+      trippleContainer(Colors.red, "A"),
+      trippleContainer(Colors.green, "B"),
+      trippleContainer(Colors.blue, "C"),
+    ],
+  );
+}
+
+Widget trippleContainer(Color color, String label) {
+  return Container(
+    width: 80,
+    height: 80,
+    color: color,
+    child: Center(
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 116, 4, 147),
+          foregroundColor: Colors.white,
+          minimumSize: Size(15, 10), // Größe des Buttons
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12), // Abrundung
+          ),
+          elevation: 2,
+        ),
+        child: Text(label),
+      ),
+    ),
+  );
+}
+
+Widget doubleFaceRow() {
+  return Row(
+    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    spacing: 25,
+    crossAxisAlignment: CrossAxisAlignment.center,
+    children: [
+      Icon(Icons.face, size: 40, color: Colors.grey),
+      Icon(Icons.face, size: 40, color: Colors.grey),
+    ],
+  );
+}
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -29,91 +89,12 @@ class MainApp extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             SizedBox(height: 10),
-            Text(
-              "Hello App Akademie!",
-              style: TextStyle(
-                color: Colors.blue,
-                fontSize: 18,
-                fontWeight: FontWeight.w900,
-                fontStyle: FontStyle.normal,
-              ),
-              textAlign: TextAlign.start,
-              textDirection: TextDirection.ltr,
-            ),
-            Row(
-              spacing: 15,
-              children: [
-                Container(
-                  width: 80,
-                  height: 80,
-                  color: Colors.red,
-                  child: Center(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 116, 4, 147),
-                        foregroundColor: Colors.white,
-                        minimumSize: Size(15, 10), // Größe des Buttons
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12), // Abrundung
-                        ),
-                        elevation: 2,
-                      ),
-                      child: Text("A"),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 80,
-                  height: 80,
-                  color: Colors.green,
-                  child: Center(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 116, 4, 147),
-                        foregroundColor: Colors.white,
-                        minimumSize: Size(15, 10), // Größe des Buttons
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12), // Abrundung
-                        ),
-                        elevation: 2,
-                      ),
-                      child: Text("B"),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 80,
-                  height: 80,
-                  color: Colors.blue,
-                  child: Center(
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color.fromARGB(255, 116, 4, 147),
-                        foregroundColor: Colors.white,
-                        minimumSize: Size(15, 10), // Größe des Buttons
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(12), // Abrundung
-                        ),
-                        elevation: 2,
-                      ),
-                      child: Text("C"),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              spacing: 50,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Icon(Icons.face, size: 40, color: Colors.grey),
-                Icon(Icons.face, size: 40, color: Colors.grey),
-              ],
-            ),
+            welcomeTextBar(),
+            welcomeTextBar(),
+            trippleContainerRow(),
+            trippleContainerRow(),
+            doubleFaceRow(),
+            doubleFaceRow(),
           ],
         ),
       ),
